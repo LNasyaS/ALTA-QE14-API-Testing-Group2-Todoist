27a3e81256ca7321eb3a7e14845bc51c8e3a9d51
LNasya Syafitrie <lnasya.syafitrie@gmail.com>
2024-03-26 04:53:24 +0700

    project starter

---
+++ src/test/java/starter/todoist/TodoistAPI.java
@@ -0,0 +1,4 @@
+package starter.todoist;
+
+public class TodoistAPI {
+}
---
+++ src/test/java/starter/utils/Constants.java
@@ -0,0 +1,8 @@
+package starter.utils;
+
+public class Constants {
+
+    public static String DIR = System.getProperty("user.dir");
+
+    public static String BASE_URL = "https://api.todoist.com/rest/v2";
+}
